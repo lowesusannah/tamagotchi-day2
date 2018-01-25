8dadbf9672df2e5cd6bf708afee110cce03b6099
Guest User <Guest@Epicodus-2A.local>
2018-01-25 12:01:14 -0800

    add styling to app

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
+import Water from '../img/water.jpg';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  componentDidMount(){
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    5000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime(){
+    console.log('check');
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+function MyStyledComponent(props) {
+  var myStyledComponentStyles = {
+    backgroundColor: 'red',
+  }
+
+
+  render(){
+    return (
+      <div >
+          <Header>
+          <Switch>
+            <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+            <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          </Switch>
+      </div>
+    );
+  }
+
+}
+
+export default App;
